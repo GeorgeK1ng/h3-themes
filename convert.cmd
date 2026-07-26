764f0d9dd00335c6a2891f87e5474ee1b0883b6f
George King <98261225+GeorgeK1ng@users.noreply.github.com>
2026-07-26 14:09:22 +0200

    Clean

--- convert.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-set "baseDir=C:\Users\Administrator\Documents\My Games\vcmi\Mods\h3-themes"
-for /r "%baseDir%" %%f in (*.bmp) do (
-    echo Converting "%%f" to PNG...
-    magick "%%f" "%%~dpnf.png"
-)
-echo Conversion completed!
-pause
